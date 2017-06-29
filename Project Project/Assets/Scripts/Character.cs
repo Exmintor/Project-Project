bda95a13f21c8e3337dbd88a3fe8e22cf42aa589
Exmintor <robertsmbj@gmail.com>
2017-06-29 23:06:52 +0000

    Characters
    Adds Characters and Character Generator

---
+++ Project Project/Assets/Scripts/Character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public string Name { get; private set; }
+    public string Race { get; private set; }
+    public string PlayerClass { get; private set; }
+    public string Background { get; private set; }
+
+    public Character(string name, string race, string playerClass, string background)
+    {
+        Name = name;
+        Race = race;
+        PlayerClass = playerClass;
+        Background = background;
+    }
+}
