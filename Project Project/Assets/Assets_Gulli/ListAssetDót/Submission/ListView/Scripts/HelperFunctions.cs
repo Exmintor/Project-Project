4d312d58e5684c0c1867d75e01d8231a53903751
nachos5 <gnorland93@gmail.com>
2017-06-29 23:19:17 +0000

    LEL

---
+++ Project Project/Assets/Assets_Gulli/ListAssetDót/Submission/ListView/Scripts/HelperFunctions.cs
@@ -0,0 +1,19 @@
+﻿namespace Endgame
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Reflection;
+
+	static class HelperFunctions
+	{
+		public static void Repeat(int count, Action action)
+		{
+			for (int i = 0; i < count; i++)
+			{
+				action();
+			}
+		}
+	}
+}
