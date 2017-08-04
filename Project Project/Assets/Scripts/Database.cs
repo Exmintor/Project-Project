31b98d0299032e5e02b1c3b95204c1abb9bb49e6
Exmintor <robertsmbj@gmail.com>
2017-08-04 23:20:02 +0000

    Reorganize
    Moves some scripts to folders and creates Database script to hold all of the objects needed for the Tab Scene

---
+++ Project Project/Assets/Scripts/Database.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Database : MonoBehaviour
+{
+    public List<Player> allFighters;
+    public List<Player> fightersToBuy;
+    public List<Player> fightersOwned;
+    public List<Scout> scoutsToBuy;
+    public List<Scout> scoutsOwned;
+    public List<Trainer> trainersToBuy;
+    public List<Trainer> trainersOwned;
+
+    //public List<Assignment> scoutAssignment;
+}
